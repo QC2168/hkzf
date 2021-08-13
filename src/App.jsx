cada2b3188c79a85feebf175fbbcd42f43ff269d
QIANCHENG2125 <huangxinhong2125@gmail.com>
2021-08-13 14:52:19 +0800

    添加配置路由

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import './App.css'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+// 导入页面组件
+import home from "./pages/home/home";
+import cityList from "./pages/cityList/cityList";
 
 function App() {
     return (
-        <div className="App">
-            根组件
-        </div>
+        <Router>
+            <div className="App">
+                {/*匹配导航菜单*/}
+                <Link to={"/home"}>首页</Link>
+                <Link to={"/cityList"}>城市列表</Link>
+                <Route path={"/home"} component={home}/>
+                <Route path={"/cityList"} component={cityList}/>
+            </div>
+        </Router>
+
     )
 }
 
---
+++ src/pages/cityList/cityList.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+export default class cityList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>城市列表</div>
+        )
+    }
+}
