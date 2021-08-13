5bcbbbfaf4c616ccaad46e35ae0430317986e25b
QIANCHENG2125 <huangxinhong2125@gmail.com>
2021-08-13 14:44:47 +0800

    引用组件库

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+//antd-mobile 样式文件
+import 'antd-mobile/dist/antd-mobile.css';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
