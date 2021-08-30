ac9f0ca4d55b524a4ac37b36d0ea77e784faf444
QIANCHENG2125 <huangxinhong2125@gmail.com>
2021-08-31 00:28:49 +0800

    地图显示小区数据

--- src/pages/home/childComponents/Map/Map.jsx
+++ src/pages/home/childComponents/Map/Map.jsx
@@ -28,6 +28,9 @@ export default class Map extends Component {
     const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'));
     // 获取地理位置信息
     const map = new BMap.Map('container');
+    // 在其他方法中可以使用
+    this.map = map;
+
     map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
     // 创建地址解析器实例
     const myGeo = new BMap.Geocoder();
@@ -40,53 +43,130 @@ export default class Map extends Component {
         //  添加组件
         map.addControl(new BMap.NavigationControl());
         map.addControl(new BMap.ScaleControl());
+        this.renderOverlays(value);
       }
-      // 获取房源信息
-      const res = await axios.get('http://127.0.0.1:8009/area/map', {
-        params: {
-          id: value,
-        },
-      });
-      res.data.body.forEach((item) => {
-      // 每个数据创建一个覆盖物
-        const {
-          coord: { longitude, latitude }, label: areaName, count, value: uValue,
-        } = item;
-        console.log(longitude, latitude);
-        //  创建label
-        const areaPoint = new BMap.Point(longitude, latitude);
-        // 设置setContent后第一个参数中设置的文本内容就失效了
-        const label1 = new BMap.Label('', {
-          position: areaPoint,
-          offset: new BMap.Size(-35, -35),
-        });
-        // 给label对象添加唯一标识
-        label1.id = uValue;
-        // 设置房源覆盖物内容
-        label1.setContent(`
-        <div class="${style.bubble}">
-          <p class="${style.name}">${areaName}</p>
-          <p>${count}套</p>
-        </div>
-      `);
-        //  设置样式
-        label1.setStyle(labelStyle);
-        map.addOverlay(label1);
-        // 添加点击时间
-        label1.addEventListener('click', () => {
-          console.log('click', label1.id);
-          // 放大地图 以中心放大
-          //   添加到地图中
-          //  第一个参数 坐标对象
-          //  第二个参数  放大级别
-          map.centerAndZoom(areaPoint, 13);
-          // 清除当前覆盖物
-          setTimeout(() => { map.clearOverlays(); }, 0);
-        });
-      });
     },
 
     label);
+  }
+
+  async renderOverlays(id) {
+    // 获取房源信息
+    const res = await axios.get('http://127.0.0.1:8009/area/map', {
+      params: {
+        id,
+      },
+    });
+    // 调用 getTypeAndZoom 方法获取级别和类型
+    const { nextZoom, type } = this.getTypeAndZoom();
+    res.data.body.forEach((item) => {
+      this.createOverlays(item, nextZoom, type);
+    });
+  }
+
+  // 区 11  镇 13 小区 15
+  getTypeAndZoom() {
+    // 调用地图
+    const zoom = this.map.getZoom();
+    let nextZoom;
+    let type;
+    if (zoom >= 10 && zoom < 12) {
+    //  区
+    //  下一个缩放级别
+      nextZoom = 13;
+      // circle 表示绘制圆形覆盖物（区、镇）
+      type = 'circle';
+    } else if (zoom >= 12 && zoom < 14) {
+    //  镇
+      nextZoom = 15;
+      type = 'circle';
+    } else if (zoom >= 14 && zoom < 16) {
+    //  小区
+      type = 'rect';
+    }
+    return {
+      nextZoom, type,
+    };
+  }
+
+  createOverlays(data, zoom, type) {
+    const {
+      coord: { longitude, latitude }, label: areaName, count, value,
+    } = data;
+    const areaPoint = new BMap.Point(longitude, latitude);
+    if (type === 'circle') {
+      this.createCricle(areaPoint, areaName, count, value, zoom);
+    } else if (type === 'rect') {
+      this.createRect(areaPoint, areaName, count, value);
+    }
+  }
+
+  // 创建区、镇覆盖物
+  createCricle(point, name, count, id, zoom) {
+    // 设置setContent后第一个参数中设置的文本内容就失效了
+    const label1 = new BMap.Label('', {
+      position: point,
+      offset: new BMap.Size(-35, -35),
+    });
+    // 给label对象添加唯一标识
+    label1.id = id;
+    // 设置房源覆盖物内容
+    label1.setContent(`
+        <div class="${style.bubble}">
+          <p class="${style.name}">${name}</p>
+          <p>${count}套</p>
+        </div>
+      `);
+    //  设置样式
+    label1.setStyle(labelStyle);
+    this.map.addOverlay(label1);
+    // 添加点击时间
+    label1.addEventListener('click', () => {
+      // 调用renderOverlays方法，获取该区域下的房源数据
+      this.renderOverlays(id);
+      // 放大地图 以中心放大
+      //   添加到地图中
+      //  第一个参数 坐标对象
+      //  第二个参数  放大级别
+      this.map.centerAndZoom(point, zoom);
+      // 清除当前覆盖物
+      setTimeout(() => { this.map.clearOverlays(); }, 0);
+    });
+  }
+
+  // 创建小区覆盖物
+  createRect(point, name, count, id) {
+    // 设置setContent后第一个参数中设置的文本内容就失效了
+    const label1 = new BMap.Label('', {
+      position: point,
+      offset: new BMap.Size(-50, -28),
+    });
+    // 给label对象添加唯一标识
+    label1.id = id;
+    // 设置房源覆盖物内容
+    label1.setContent(`
+        <div class="${style.rect}">
+          <span class="${style.housename}">${name}</span>
+          <span class="${style.housenum}">${count}套</span>
+          <i class="${style.arrow}"></i>
+        </div>
+
+      `);
+    //  设置样式
+    label1.setStyle(labelStyle);
+    this.map.addOverlay(label1);
+    // 添加点击时间
+    label1.addEventListener('click', () => {
+      // 调用renderOverlays方法，获取该区域下的房源数据
+      this.renderOverlays(id);
+      // 放大地图 以中心放大
+      //   添加到地图中
+      //  第一个参数 坐标对象
+      //  第二个参数  放大级别
+      this.map.centerAndZoom(point, zoom);
+      // 清除当前覆盖物
+      setTimeout(() => { this.map.clearOverlays(); }, 0);
+    });
   }
 
   render() {
