d8b1298786d5c8a4f8b07610d6b8c3e70d861111
QIANCHENG2125 <huangxinhong2125@gmail.com>
2021-09-23 21:15:10 +0800

    城市切换bug

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -11,9 +11,10 @@ import { BASE_URL } from '../../utils/url';
 import HouseItem from '../../components/HouseItem/HouseItem';
 import Sticky from '../../components/Sticky/Sticky';
 import NoHouse from '../../components/NoHouse/NoHouse';
+import { getCurrentCity } from '../../utils';
 
 // 获取当前定位
-const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'));
+// const { label, value } = JSON.parse(localStorage.getItem('hkzf_city'));
 export default class Index extends Component {
   constructor(props) {
     super(props);
@@ -28,10 +29,17 @@ export default class Index extends Component {
     this.filters = {};
   }
 
-  // eslint-disable-next-line camelcase
-  componentDidMount() {
-    this.searchHouseList();
-  }
+    label = ''
+
+    value = ''
+
+    // eslint-disable-next-line camelcase
+    async componentDidMount() {
+      const { label, value } = await getCurrentCity();
+      this.label = label;
+      this.value = value;
+      this.searchHouseList();
+    }
 
     // 函数返回值代表最终渲染页面的内容
     renderHouseList = ({
@@ -69,131 +77,131 @@ export default class Index extends Component {
       this.searchHouseList();
     }
 
-  //  判断列表中每一行是否加载完成
-  isRowLoaded=({ index }) => {
-    const { housesList } = this.state;
-    return !!housesList[index]; //
-  }
-
-  // 用来获取更多房屋列表数据
-  loadMoreRows=({ startIndex, stopIndex }) => {
-    console.log(startIndex, stopIndex);
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve) => {
-      //  数据加载完成时，调用res
-      API.get('/houses', {
-        params: {
-          cityId: value,
-          ...this.filters,
-          start: startIndex,
-          end: stopIndex,
-        },
-      }).then((res) => {
-        console.log(res);
-        const { housesList } = this.state;
-        const { list } = res;
-        this.setState({
-          housesList: [...housesList, ...list],
-          isloading: false,
-        });
-        // 调用resolve
-        resolve();
-      });
-    });
-  }
-
-  // 用于获取房屋数据
-  async searchHouseList() {
-    // 开启loading
-    Toast.loading('加载中...', 0, null, false);
-    const res = await API.get('/houses', {
-      params: {
-        cityId: value,
-        ...this.filters,
-        start: 1,
-        end: 20,
-      },
-    });
-    Toast.hide();
-    // 显示房源
-    const { list, count } = res;
-    if (count !== 0) {
-      Toast.info(`共找到${count}套房源`, 2, null, false);
+    //  判断列表中每一行是否加载完成
+    isRowLoaded = ({ index }) => {
+      const { housesList } = this.state;
+      return !!housesList[index]; //
     }
-    this.setState({
-      housesList: list,
-      count,
-      isloading: false,
-    });
-  }
 
-  renderList() {
-    const { count, isloading } = this.state;
-    if (count === 0 && !isloading) return <NoHouse>没有找到房源，请您换个搜索条件吧 ~</NoHouse>;
-    return (
-      <InfiniteLoader
-        isRowLoaded={this.isRowLoaded}
-        loadMoreRows={this.loadMoreRows}
-        rowCount={count}
-      >
-        {
-            ({ onRowsRendered, registerChild }) => (
-              <WindowScroller>
-                {({ height, isSrcolling, scrollTop }) => (
-                  <AutoSizer>
-                    {
-                          ({ width }) => (
-                            <List
-                                  // 设置高度为windowScroller最终渲染的列表高度
-                              autoHeight
-                                  // 视口的宽度
-                              width={width}
-                                  // 视口的高度
-                              height={height}
-                                  // 列表的行数
-                              rowCount={count}
-                                  // 每一行的高度
-                              rowHeight={120}
-                              rowRenderer={this.renderHouseList}
-                              isScrolling={isSrcolling}
-                              scrllTop={scrollTop}
-                              ref={registerChild}
-                              onRowsRendered={onRowsRendered}
-                            />
-                          )
-                        }
-                  </AutoSizer>
-                )}
-              </WindowScroller>
-            )
-          }
-      </InfiniteLoader>
-    );
-  }
+    // 用来获取更多房屋列表数据
+    loadMoreRows = ({ startIndex, stopIndex }) => {
+      console.log(startIndex, stopIndex);
+      // eslint-disable-next-line no-unused-vars
+      return new Promise((resolve) => {
+        //  数据加载完成时，调用res
+        API.get('/houses', {
+          params: {
+            cityId: this.value,
+            ...this.filters,
+            start: startIndex,
+            end: stopIndex,
+          },
+        }).then((res) => {
+          console.log(res);
+          const { housesList } = this.state;
+          const { list } = res;
+          this.setState({
+            housesList: [...housesList, ...list],
+            isloading: false,
+          });
+          // 调用resolve
+          resolve();
+        });
+      });
+    }
 
-  render() {
-    const { history } = this.props;
-    return (
-      <div className="List">
-        <Flex className={styles.header}>
-          <i
-            className="iconfont icon-arrow-left"
-            onClick={() => {
-              history.go(-1);
-            }}
-          />
-          <SearchHeader cityName={label} className={styles.searchHeader} />
-        </Flex>
-        {/* 选择器 */}
-        <Sticky height={40}>
-          <Filter onFilter={this.onFilter} />
-        </Sticky>
+    // 用于获取房屋数据
+    async searchHouseList() {
+      // 开启loading
+      Toast.loading('加载中...', 0, null, false);
+      const res = await API.get('/houses', {
+        params: {
+          cityId: this.value,
+          ...this.filters,
+          start: 1,
+          end: 20,
+        },
+      });
+      Toast.hide();
+      // 显示房源
+      const { list, count } = res;
+      if (count !== 0) {
+        Toast.info(`共找到${count}套房源`, 2, null, false);
+      }
+      this.setState({
+        housesList: list,
+        count,
+        isloading: false,
+      });
+    }
 
-        {/*  房屋列表 */}
-        <div className={styles.houseItems}>
-          {this.renderList()}
+    renderList() {
+      const { count, isloading } = this.state;
+      if (count === 0 && !isloading) return <NoHouse>没有找到房源，请您换个搜索条件吧 ~</NoHouse>;
+      return (
+        <InfiniteLoader
+          isRowLoaded={this.isRowLoaded}
+          loadMoreRows={this.loadMoreRows}
+          rowCount={count}
+        >
+          {
+                    ({ onRowsRendered, registerChild }) => (
+                      <WindowScroller>
+                        {({ height, isSrcolling, scrollTop }) => (
+                          <AutoSizer>
+                            {
+                                        ({ width }) => (
+                                          <List
+                                                // 设置高度为windowScroller最终渲染的列表高度
+                                            autoHeight
+                                                // 视口的宽度
+                                            width={width}
+                                                // 视口的高度
+                                            height={height}
+                                                // 列表的行数
+                                            rowCount={count}
+                                                // 每一行的高度
+                                            rowHeight={120}
+                                            rowRenderer={this.renderHouseList}
+                                            isScrolling={isSrcolling}
+                                            scrllTop={scrollTop}
+                                            ref={registerChild}
+                                            onRowsRendered={onRowsRendered}
+                                          />
+                                        )
+                                    }
+                          </AutoSizer>
+                        )}
+                      </WindowScroller>
+                    )
+                }
+        </InfiniteLoader>
+      );
+    }
+
+    render() {
+      const { history } = this.props;
+      return (
+        <div className="List">
+          <Flex className={styles.header}>
+            <i
+              className="iconfont icon-arrow-left"
+              onClick={() => {
+                history.go(-1);
+              }}
+            />
+            <SearchHeader cityName={this.label} className={styles.searchHeader} />
+          </Flex>
+          {/* 选择器 */}
+          <Sticky height={40}>
+            <Filter onFilter={this.onFilter} />
+          </Sticky>
+
+          {/*  房屋列表 */}
+          <div className={styles.houseItems}>
+            {this.renderList()}
+          </div>
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
